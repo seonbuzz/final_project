ff480d19082c3cf5fbc325b738d1d5464121ef03
seonbuzz <ddonsdevelop@gmail.com>
2023-02-15 11:26:51 +0900

    event db

---
+++ docs/db/event/event_DML.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO EVENT (EVENT_UID, TITLE,CONTENT) 
+ VALUES ('E1','2023 내 차 사기 퀴즈 이벤트','이벤트 내용이 궁금하다면?');
+
+INSERT INTO EVENT (EVENT_UID, TITLE,CONTENT) 
+ VALUES ('E2','2023 차량등록 이벤트','차량번호 입력하고 공기청정기 받자!');
---
+++ docs/db/event/event_page_DML.sql
@@ -0,0 +1,5 @@
+INSERT INTO EVENT_PAGE (EVENT_UID, TITLE, DATE, CONTENT,ING_END) 
+ VALUES ('E1','2023 내 차 사기 퀴즈 이벤트','2023.02.02~2023.03.02','자세한 내용은 이미지를 참고 바랍니다.', '진행중');
+
+INSERT INTO EVENT_PAGE (EVENT_UID, TITLE, DATE, CONTENT,ING_END) 
+ VALUES ('E2','2023 차량등록 이벤트','2023.01.01~2023.03.01','자세한 내용은 이미지를 참고 바랍니다.', '진행중');
